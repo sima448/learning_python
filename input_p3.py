c54b3828f76fe6a74f283334d1e68a5308afc730
Sima Kumari Singh <simak4484@gmail.com>
2025-10-11 00:17:30 +0530

    Add files via upload

---
+++ input_p3.py
@@ -0,0 +1,15 @@
+#input in python
+# #string input
+name = input("name: ")
+print(name)
+name1 = input("name1 : Suman")
+
+#int input
+age = int(input("age : "))
+print(age)
+
+#float input
+price = float(input("price : ")) 
+
+
+print("My name is", name, "and I am", age, "years old.")
